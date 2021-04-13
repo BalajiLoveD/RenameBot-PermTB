13feab53c2328dc90ee5a354ba84677f8c1e82e7
BalajiLoveD <62749324+BalajiLoveD@users.noreply.github.com>
2021-04-13 15:34:34 +0530

    Update bruh.py

--- plugins/bruh.py
+++ plugins/bruh.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, Filters
 
 
-@Client.on_message(Filters.command(["help"]))
+@Client.on_message(Filters.command(["bruh"]))
 async def start(client, message):
     helptxt = f"What Do You Want Bruh.....😂 If You Need Anything Contact @BLuVDS"
     await message.reply_text(helptxt)
